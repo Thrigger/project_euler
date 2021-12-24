b93d88b52b4fd7c810d9cf6c5ce05c5e316724dc
Thrigger <21341498+Thrigger@users.noreply.github.com>
2021-12-24 11:03:21 +0100

    Updates support libs

--- src/problems.rs
+++ src/problems.rs
@@ -9,18 +9,21 @@ mod p007;
 mod p010;
 mod p011;
 
+mod p029;
+
 pub fn solve(problem: i32, verbose: bool) -> u64 {
     let solution = match problem {
-        1 => p001::solve(),
-        2 => p002::solve(),
-        3 => p003::solve(),
-        4 => p004::solve(),
-        5 => p005::solve(),
-        6 => p006::solve(),
-        7 => p007::solve(),
-        10 => p010::solve(),
-        11 => p011::solve(),
-        _ => 0,
+        1   => p001::solve(),
+        2   => p002::solve(),
+        3   => p003::solve(),
+        4   => p004::solve(),
+        5   => p005::solve(),
+        6   => p006::solve(),
+        7   => p007::solve(),
+        10  => p010::solve(),
+        11  => p011::solve(),
+        29  => p029::solve(),
+        _   => 0,
     };
 
     solution
